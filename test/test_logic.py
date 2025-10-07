cce330da062fe955709fc799b34c8108fab3cb83
Bruno Lage <sonbruno1@gmail.com>
2025-10-06 22:22:44 -0300

    v0.1 - quickfix to tests structure
    Added:
    
    - test_utils.py and test_logic.py division
    
    Changed:
    
    - separated utils and logic functions into their respective test files
    
    Removed:
    
    None

--- test/test_logic.py
+++ test/test_logic.py
@@ -1,39 +1,17 @@
+"""Tested functions
+
+- check_row
+- check_col
+- check_sqr
+- check_board
+"""
+
 import unittest
 
-from utils import has_nonzero_duplicate
 from logic import check_row, check_col, check_sqr, check_board
 
 
-class TestUtils(unittest.TestCase):
-
-    def test_has_nonzero_duplicate(self):
-        # Case 1: A list with no duplicates
-        no_duplicates = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.assertFalse(
-            has_nonzero_duplicate(no_duplicates),
-            "Should be False for a list with no duplicates",
-        )
-
-        # Case 2: A list with duplicates
-        with_duplicates = [1, 2, 3, 4, 5, 1, 7, 8, 9]
-        self.assertTrue(
-            has_nonzero_duplicate(with_duplicates),
-            "Should be True for a list with duplicates",
-        )
-
-        # Case 3: A list with zeroes and no other duplicates
-        with_zeroes = [0, 1, 2, 3, 0, 4, 5, 6, 7]
-        self.assertFalse(
-            has_nonzero_duplicate(with_zeroes),
-            "Should be False as zeroes are ignored",
-        )
-
-        # Case 4: A list with zeroes and other duplicates
-        with_zeroes_and_duplicates = [0, 1, 2, 1, 0, 4, 5, 6, 7]
-        self.assertTrue(
-            has_nonzero_duplicate(with_zeroes_and_duplicates),
-            "Should be True for duplicates even with zeroes present",
-        )
+class TestLogic(unittest.TestCase):
 
     def test_check_row(self):
         # Case 1: A list with no duplicates
---
+++ test/test_utils.py
@@ -0,0 +1,44 @@
+"""Tested functions
+
+- has_nonzero_duplicate
+"""
+
+import unittest
+
+from utils import has_nonzero_duplicate
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_has_nonzero_duplicate(self):
+        # Case 1: A list with no duplicates
+        no_duplicates = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertFalse(
+            has_nonzero_duplicate(no_duplicates),
+            "Should be False for a list with no duplicates",
+        )
+
+        # Case 2: A list with duplicates
+        with_duplicates = [1, 2, 3, 4, 5, 1, 7, 8, 9]
+        self.assertTrue(
+            has_nonzero_duplicate(with_duplicates),
+            "Should be True for a list with duplicates",
+        )
+
+        # Case 3: A list with zeroes and no other duplicates
+        with_zeroes = [0, 1, 2, 3, 0, 4, 5, 6, 7]
+        self.assertFalse(
+            has_nonzero_duplicate(with_zeroes),
+            "Should be False as zeroes are ignored",
+        )
+
+        # Case 4: A list with zeroes and other duplicates
+        with_zeroes_and_duplicates = [0, 1, 2, 1, 0, 4, 5, 6, 7]
+        self.assertTrue(
+            has_nonzero_duplicate(with_zeroes_and_duplicates),
+            "Should be True for duplicates even with zeroes present",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()

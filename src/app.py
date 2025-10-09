f352f84f9012a3b4ccacbfb2e63c9666d38ffbb2
Bruno Lage <sonbruno1@gmail.com>
2025-10-09 16:56:27 -0300

    v0.2 - started integrating app.py with board.py
    Added:
    
    - now app.py initializes a Board instance and loads a state.
    
    Changed:
    
    None
    
    Removed:
    
    None

--- src/app.py
+++ src/app.py
@@ -8,6 +8,8 @@ The UI is made in the sudoku.kv file, so this file is just a wrapper.
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from board import Board
+from utils import EXAMPLE_BOARD
 
 
 class SudokuGrid(GridLayout):
@@ -15,11 +17,16 @@ class SudokuGrid(GridLayout):
 
 
 class SudokuApp(App):
-    def on_start(self):
+    def build(self):
+
+        self.board = Board(EXAMPLE_BOARD)
+
         sudoku_grid = self.root.ids.sudoku_grid
         for i in range(9):
             for j in range(9):
-                cell = Button(text=f"{i},{j}")
+                number = self.board.state[i][j]
+                num_text = str(number) if number != 0 else ""
+                cell = Button(text=num_text)
                 sudoku_grid.add_widget(cell)
 
 
